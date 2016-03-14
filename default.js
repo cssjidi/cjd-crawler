d5a7bbc9c47245a8564e61f15be39546f490d576
cssjidi <admin@cssjidi.com>
2016-03-14 23:20:38 +0800

    Update default.js

--- default.js
+++ default.js
@@ -14,21 +14,24 @@ var Spider = function(url,options){
         return false;
     }
     this.config = {};
-    this.hrefLinks = [];
-    this.imgLinks = [];
+    //this.hrefLinks = [];
+    //this.imgLinks = [];
+
     if (typeof options === 'object') {
-        extend(config, options, {uri: url})
+        extend(this.config, options, {uri: url})
     } else if (typeof url === 'string') {
-        extend(config, {url: url})
+        extend(this.config, {url: url})
     } else {
-        extend(config, url)
+        extend(this.config, url)
     }
+    this.rootUrl = function (i) { return this.config.url.replace('%d', i);};
     if(url === undefined){
         return false;
     }
 }
 //处理入口
 Spider.prototype.init = function(){
+    this.crawler();
 
 }
 //处理第一层的链接(分页)
@@ -41,3 +44,97 @@ Spider.prototype.urls = function(){
         hrefLinks.push();
     }
 }
+//处理层级
+Spider.prototype.crawler = function(){
+    var self = this;
+    var urlLevls = [];
+    console.log('程序执行中');
+    async.eachSeries(this.config.selector,function(item,callback){
+        var index = self.config.selector.indexOf(item);
+        if(index === 0){
+            urlLevls[0] = [];
+            var i = self.config.from;
+            async.whilst(function(){
+                return i <= self.config.to;
+            },function(_callback){
+                //urlLevls.push(this.config)
+                console.log(i);
+                self.request(self.rootUrl(i),function(status,$){
+
+                    if(status){
+                        var $$ = eval(item.$);
+                        $$.each(function(){
+                            var nextUrl = $(this).attr(item.attr);
+                            urlLevels[0].push(nextUrl);
+                        });
+                    }
+                    setTimeout(function () {
+                        ++i;
+                        _callback(null);
+                    }, parseInt(Math.random() * 5000));
+                })
+            }, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('分页处理完成，共收集到了' + urlLevels[0].length);
+                }
+                console.log(urlLevls);
+                callback(null);
+            })
+        }
+    },function(err){
+        if(err){
+
+        }
+    })
+}
+//重写request方法
+Spider.prototype.request = function(url,callback){
+    var self = this;
+    var options = {
+        url:url
+    }
+    var that = this;
+    var opts = {
+        url: url,
+        encoding: null /// 设置为null时，得到的body为buffer类型
+    };
+    //this.config.header && (options.headers = this.config.headers);
+    /*request(options,function(err,res,body){
+        var $ = null;
+        if(!err && res.statusCode === 200){
+            console.log('状态:'+res.statusCode+','+url+';请求成功');
+            $ = cheerio.load(iconv.decode(body,this.config.chartset || 'utf8'));
+        }else{
+            console.log('状态:'+res.statusCode+','+url+';请求失败');
+        }
+        callback(!!$,$);
+    });
+    config.headers && (opts.headers = config.headers);*/
+
+    console.log('发送' + url + '，等待响应中...');
+    request(opts, function (err, res, body) {
+        var $ = null;
+        if (!err && res.statusCode == 200) {
+            that.log('状态' + res.statusCode + '， ' + url + '请求成功', 'green');
+            $ = cheerio.load(iconv.decode(body, config.charset || 'utf8'));
+        } else {
+            !err && that.log('状态' + res.statusCode + '， ' + url + '请求失败', 'red');
+        }
+        callback(!!$, $);
+    });
+}
+
+var spider = new Spider('http://www.henha.com/',{
+    from:1,
+    to:10,
+    selector:[{
+        $:'$(".detail-list li")',
+        attr:'href'
+    },{
+        $:'$(".pp img")',
+        attr:'src'
+    }]
+});
+spider.init();
